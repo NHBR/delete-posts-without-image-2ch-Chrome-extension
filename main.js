5fdcd49abd7cfd91483b9bb83c3d225a4ee6230f
NHBR <opa228stail@gmail.com>
2020-11-30 20:30:59 +0300

    added UI, changed manifest,added jquery min

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let elems = document.getElementsByClassName('thread__post');
-for (i = elems.length; i >0 ; i--){
-    if (elems[i-1].getElementsByClassName('post__image-link').length === 0){
-
-        elems[i-1].remove();  
-    
-    }
+let deleter = document.getElementById('deleter');
+if(deleter != null) {
+  deleter.onclick = function(el) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id, 
+        {code: 'let elems = document.getElementsByClassName("thread__post"); for (i = elems.length; i >0 ; i--){if (elems[i-1].getElementsByClassName("post__image-link").length === 0){elems[i-1].remove();}}'}
+      );
+    })
+  }
 }
